5ddc2b7c3508348e59be9f39e25a587c5acfab1b
Marc Satchmo <marcsatchmo@gmail.com>
2023-06-21 22:09:14 -0300

    inicial

---
+++ POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using POS.Infrastructure.Persistences.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assembly = typeof(POSContext).Assembly.FullName;
+            services.AddDbContext<POSContext>(
+                options => options.UseSqlServer(
+                    configuration.GetConnectionString("POSConnection"), b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient);
+            return services;
+        }
+    }
+}
---
+++ POS.Infrastructure/Persistences/Contexts/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations
+{
+    public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(e => e.Name).HasMaxLength(100);
+        }
+    }
+}
